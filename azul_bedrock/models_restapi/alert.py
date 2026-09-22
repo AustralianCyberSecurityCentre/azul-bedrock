4242579a313178ba5a48e366b4cab832fdabdc90
dan-acsc <dan-acsc@users.noreply.github.com>
2026-09-22 03:44:36 +0000

    feat: add alerter to raise alerts for certain events in dispatcher.

---
+++ azul_bedrock/models_restapi/alert.py
@@ -0,0 +1,44 @@
+"""Models for alert endpoints and redis storage."""
+
+import datetime
+
+from pydantic import BaseModel, ConfigDict
+
+from azul_bedrock.models_network import BinaryAction
+
+# All models here have to map to gosrc/models/alert.go
+
+# Redis key for the storing of the alerters configuration.
+ALERTER_CONFIG_KEY = "config"
+# Redis key for the storing of active alerts in redis
+ALERTER_ALERT_KEY = "alerts"
+# Redis Alerter database ID.
+ALERTER_DB_ID = 4
+
+
+class AlertRule(BaseModel):
+    """Alert rule."""
+
+    model_config = ConfigDict(use_enum_values=True)
+
+    alert_endpoint: str
+    event_type: BinaryAction
+    plugin_name: str
+    plugin_version: str
+    source_name: str
+    source_reference_key_values: dict[str, str]
+    feature_name_values: dict[str, str]
+
+
+class LoadedRules(BaseModel):
+    """Loaded rules that are stored in redis."""
+
+    rules: list[AlertRule]
+    rules_compile_time: datetime.datetime
+
+
+class AlertHit(BaseModel):
+    """Alert hit that can be stored in redis."""
+
+    rule: AlertRule
+    sha256: str
---
+++ gosrc/models/alert.go
@@ -0,0 +1,41 @@
+package models
+
+import (
+	"time"
+
+	"github.com/AustralianCyberSecurityCentre/azul-bedrock/v13/gosrc/events"
+)
+
+// All models here have to map to models_restapi/alert.py
+
+// Redis key for the storing of the alerters configuration.
+const ALERTER_CONFIG_KEY = "config"
+
+// Redis key for the storing of active alerts in redis
+const ALERTER_ALERT_KEY = "alerts"
+
+// Redis Alerter database ID.
+const ALERTER_DB_ID = 4
+
+// Alert rule used to match against events, if a match is achieved an alert is raised.
+type AlertRule struct {
+	AlertEndpoint            string              `json:"alert_endpoint"`
+	EventType                events.BinaryAction `json:"event_type"`
+	PluginName               string              `json:"plugin_name"`
+	PluginVersion            string              `json:"plugin_version"`
+	SourceName               string              `json:"source_name"`
+	SourceReferenceKeyValues map[string]string   `json:"source_reference_key_values"`
+	FeatureNameValues        map[string]string   `json:"feature_name_values"`
+}
+
+// All loaded alert rules stored in Redis with the compile time so endpoints can check if there are any new rules.
+type LoadedRules struct {
+	Rules            []AlertRule `json:"rules"`
+	RulesCompileTime time.Time   `json:"rules_compile_time"`
+}
+
+// Format for an alert hit that should be stored in redis when an alert hit is made.
+type AlertHit struct {
+	Rule   AlertRule `json:"rule"`
+	Sha256 string    `json:"sha256"`
+}
